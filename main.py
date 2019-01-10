5f99c06e0b393dd0ba5470ab3ca5f4e8b3899bc2
Viktor Rosvall <virosen@yahoo.se>
2019-01-11 00:21:57 +0100

    Fixed typo and removed commented code

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# /home/simon/.mozilla/firefox/f64wwqg7.default/
-# https://cas2.miun.se/cas/login?service=https%3A%2F%2Fportal.miun.se%2Fc%2Fportal%2Flogin%3Fredirect%3D%252Fgroup%252Fstudent%26p_l_id%3D10634
-# http://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php 
-# https://medium.freecodecamp.org/better-web-scraping-in-python-with-selenium-beautiful-soup-and-pandas-d6390592e251
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
@@ -13,14 +8,10 @@ import time
 import config 
 
 ## Selenium for chromium
-from selenium import webdriver
 browser = webdriver.Chrome("./chromedriver") # Replace with .Firefox()
 url = "https://portal.miun.se/group/student/my-schedule?p_p_id=miunmyscheduleportlet_WAR_miunmyscheduleportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_miunmyscheduleportlet_WAR_miunmyscheduleportlet_tabs1=grouproom" 
 browser.get(url) # Try to navigate to schedule page
 
-##selenium for firefox
-# profile = FirefoxProfile("/home/simon/.mozilla/firefox/jjen2epc.selenium") ##select folder for firefox profile
-# browser = webdriver.Firefox(firefox_profile=profile) #load the firefox driver with the profile
 
 ### LOGIN
 usernameBox = browser.find_element_by_id("username") # Username form field
@@ -32,7 +23,7 @@ passwordBox.send_keys(config.password)
 submitButton = browser.find_element_by_name("submit") 
 submitButton.click() 
 
-### SELECT CAMPUT
+### SELECT CAMPUS
 select = Select(browser.find_element_by_id('_miunmyscheduleportlet_WAR_miunmyscheduleportlet_pCampus'))
 select.select_by_visible_text("Sundsvall")
 
