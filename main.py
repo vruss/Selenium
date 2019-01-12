408d21d946b747b2d2ee9553ed0799833bf2506a
Viktor Rosvall <virosen@yahoo.se>
2019-01-12 14:41:53 +0100

    removed unused imports

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import sys
 from json import JSONEncoder
 import json
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support.ui import Select
 import time
 import argparse
